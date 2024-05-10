7839213c8116291b3bcc444a65e9d91afbe16bf8
saltcam <saltcam@uw.edu>
2024-05-09 19:33:29 -0700

    Rewrote minimax to be more efficient. Honestly don't know how else to write a new selectMove method even after arduous reading.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -1,66 +1,51 @@
 // Tic Tac Toe
 class Agent {
-    constructor() {
+    constructor() {}
 
-    }
-
+    // check a board state for X, then all board states for O, then all for X, etc. until we hit a board that results in a 1
+    // if a board can let X win, but we know we can't reach it because O gets in the way, its a draw.
+    // once we recurse all the way through, we pick a board that results in a 1. if there are no 1s, pick a draw. if no draws, suffer.
+    // Dynamically program a base case to end the calls. Recurse until we hit it.
     minimax(board, isMaximizing) {
-        // Base cases - check if the game is over or a draw
-        var gameOver = board.gameOver();
-        if (gameOver === 1) {
-            return 1; // X wins
-        } else if (gameOver === 2) {
-            return -1; // O wins
-        } else if (gameOver === 3) {
-            return 0; // the game is a draw
-        }
 
-        // Recursive case - evaluate all possible moves and choose the best score
-        if (isMaximizing) {
-            var bestScore = -Infinity;
-            for (var i = 0; i < board.cells.length; i++) {
-                var cell = i + 1;
-                if (board.cellFree(cell)) {
-                    var newBoard = board.clone();
-                    newBoard.move(cell);
-                    var score = this.minimax(newBoard, false);
-                    bestScore = Math.max(bestScore, score);
-                }
-            }
-            return bestScore;
-        } else {
-            var bestScore = Infinity;
-            for (var i = 0; i < board.cells.length; i++) {
-                var cell = i + 1;
-                if (board.cellFree(cell)) {
-                    var newBoard = board.clone();
-                    newBoard.move(cell);
-                    var score = this.minimax(newBoard, true);
-                    bestScore = Math.min(bestScore, score);
-                }
-            }
-            return bestScore;
+        let gameOver = board.gameOver();
+        switch(gameOver) {
+            case 1: return 1;
+            case 2: return -1;
+            case 3: return 0;
         }
+        var bestScore = isMaximizing ? -Infinity : Infinity;
+        for (let i = 0; i < board.cells.length; i++) {
+            let cell = i + 1;
+            if (board.cellFree(cell)) {
+                let newBoard = board.clone();
+                newBoard.move(cell);
+                let score = this.minimax(newBoard, !isMaximizing);
+                bestScore = isMaximizing ? Math.max(bestScore, score): Math.min(bestScore, score);
+            }
+        }
+        return bestScore;
     }
 
+    // honestly not sure how else I would do this...
     selectMove(board) {
-        // Define the initial best score and move
-        var maxScore = -Infinity;
-        var maxMove = null;
+    // Define the initial best score and move
+        let maxScore = -Infinity;
+        let maxMove = null;
 
-        var minScore = Infinity;
-        var minMove = null;
+        let minScore = Infinity;
+        let minMove = null;
 
         // Loop through each cell to evaluate the best move
-        for (var i = 0; i < board.cells.length; i++) {
-            var cell = i + 1;
+        for (let i = 0; i < board.cells.length; i++) {
+            let cell = i + 1;
             if (board.cellFree(cell)) {
                 // Make a move on the current cell
-                var newBoard = board.clone();
+                let newBoard = board.clone();
                 newBoard.move(cell);
 
                 // Calculate the score for the current move
-                var score = this.minimax(newBoard, !board.playerOne);
+                let score = this.minimax(newBoard, !board.playerOne);
 
                 // Update the best move if the current move has a higher score
                 if (score > maxScore) {
@@ -73,7 +58,6 @@ class Agent {
                 }
             }
         }
-
         return board.playerOne ? maxMove : minMove;
     }
 
